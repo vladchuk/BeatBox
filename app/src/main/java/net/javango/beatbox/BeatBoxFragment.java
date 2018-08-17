2d730397e2e459052ffde8a291efad6287ebadf6
vladchuk <vladchuk@javango.net>
2018-08-17 16:52:10 -0700

    Initial commit.

---
+++ app/src/main/java/net/javango/beatbox/BeatBoxFragment.java
@@ -0,0 +1,10 @@
+package net.javango.beatbox;
+
+import android.support.v4.app.Fragment;
+
+public class BeatBoxFragment extends Fragment {
+
+    public static BeatBoxFragment newInstance() {
+        return new BeatBoxFragment();
+    }
+}
