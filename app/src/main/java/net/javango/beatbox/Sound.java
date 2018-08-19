0c9191f5da075b3440b1cca2c9fdad072f4ce31b
vladchuk <vladchuk@javango.net>
2018-08-19 12:17:46 -0700

    Ch 20: added view model and sound assets.

---
+++ app/src/main/java/net/javango/beatbox/Sound.java
@@ -0,0 +1,24 @@
+package net.javango.beatbox;
+
+import java.io.File;
+import java.nio.file.Files;
+
+public class Sound {
+    private String mAssetPath;
+    private String mName;
+
+    public Sound(String assetPath) {
+        mAssetPath = assetPath;
+        String[] components = assetPath.split("/");
+        String filename = components[components.length - 1];
+        mName = filename.replace(".wav", "");
+    }
+
+    public String getAssetPath() {
+        return mAssetPath;
+    }
+
+    public String getName() {
+        return mName;
+    }
+}
